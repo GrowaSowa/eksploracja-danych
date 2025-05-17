e2d369eb283c693bd2454b58b0418758d59e9f83
GrowaSowa <growasowa@gmail.com>
2025-05-17 19:07:56 +0200

    added review loading functions

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import sys
+import os
+import random
 
 def load_ratings()->pd.DataFrame:
 	ratings = pd.read_csv('./data/rating.csv',
@@ -58,3 +61,29 @@ def load_genome_scores()->pd.DataFrame:
 			'tagId': np.int64,
 			'relevance': np.float64
 		})
+
+def load_positive(n: int)->list[str]:
+	train_pos = ['./data/aclImdb/train/pos/' + fname for fname in os.listdir('./data/aclImdb/train/pos')]
+	test_pos = ['./data/aclImdb/test/pos/'+fname for fname in os.listdir('./data/aclImdb/test/pos')]
+
+	positive = train_pos.copy()
+	positive.extend(test_pos)
+	random.shuffle(positive)
+	
+	content = [get_content(fname) for fname in positive[:n]]
+	return content
+
+def load_negative(n: int)->list[str]:
+	train_neg = ['./data/aclImdb/train/neg/' + fname for fname in os.listdir('./data/aclImdb/train/neg')]
+	test_neg = ['./data/aclImdb/test/neg/' + fname for fname in os.listdir('./data/aclImdb/test/neg')]
+
+	negative = train_neg.copy()
+	negative.extend(test_neg)
+	random.shuffle(negative)
+
+	content = [get_content(fname) for fname in negative[:n]]
+	return content
+
+def get_content(fname: str)->str:
+	with open(fname, 'r') as f:
+		return ' '.join(f.readlines())
