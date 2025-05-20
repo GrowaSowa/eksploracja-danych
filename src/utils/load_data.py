d96697d58de936177e40a86f973ad0addd1a88bf
GrowaSowa <growasowa@gmail.com>
2025-05-20 12:46:39 +0200

    slightly improved data loading

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -4,11 +4,17 @@ import sys
 import os
 import random
 
+def load_reviews(n: int)->dict[str, list[str]]:
+	return {
+		"negative": load_negative(n),
+		"positive": load_positive(n)
+	}
+
 def load_positive(n: int)->list[str]:
 	train_pos = ['./data/aclImdb/train/pos/' + fname for fname in os.listdir('./data/aclImdb/train/pos')]
 	test_pos = ['./data/aclImdb/test/pos/'+fname for fname in os.listdir('./data/aclImdb/test/pos')]
 
-	positive = train_pos.copy()
+	positive = train_pos
 	positive.extend(test_pos)
 	random.shuffle(positive)
 
@@ -19,7 +25,7 @@ def load_negative(n: int)->list[str]:
 	train_neg = ['./data/aclImdb/train/neg/' + fname for fname in os.listdir('./data/aclImdb/train/neg')]
 	test_neg = ['./data/aclImdb/test/neg/' + fname for fname in os.listdir('./data/aclImdb/test/neg')]
 
-	negative = train_neg.copy()
+	negative = train_neg
 	negative.extend(test_neg)
 	random.shuffle(negative)
 
