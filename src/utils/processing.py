1af9d2ca2a19905fb4c96f516edb44e5160f996c
GrowaSowa <growasowa@gmail.com>
2025-05-20 11:27:40 +0200

    added dot to stopwords

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -6,7 +6,7 @@ from nltk.stem import WordNetLemmatizer
 REMOVE_RE = re.compile(' *<br \/> *')
 SEPARATE_RE = re.compile(' *(\.|!\?|!|\?|,) *')
 SPACES_RE = re.compile(' {2,}')
-ADDITIONAL_STOPWORDS = ['movie', 'film', ',', '-']
+ADDITIONAL_STOPWORDS = ['movie', 'film', ',', '-', '.']
 
 def split_unigrams(data: dict[str, list[str]])->dict[str, list[list[str]]]:
 	stopwords = get_stopwords()
